7a0fe65e1c55adb2b27fbb7ddeeaf44255663e07
matte887 <m.erroi92@gmail.com>
2022-04-27 16:02:36 +0200

    Correzioni + refactoring

--- js/style.js
+++ js/style.js
@@ -2,14 +2,14 @@
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-// Creo cinque numeri casuali da 1 a 100 e li stampo in pagina.
+// Creo cinque numeri casuali da 1 a 100 non ripetuti e li stampo in pagina.
 let gameNumbers = [];
 
-while (gameNumbers.length <= 5) {
+while (gameNumbers.length <= 4) {
     const randomNumber = getRndInteger(1, 100);
     if (!gameNumbers.includes(randomNumber)) {
         gameNumbers.push(randomNumber);
-    }
+    };
 };
 
 const numbersContainer = document.getElementById("numbers");
@@ -21,27 +21,30 @@ setTimeout(function(){
     numbersContainer.classList.add("hide");
 }, 3000);
 
-// Chiedo all'utente i numeri
 setTimeout(function(){
+    // Chiedo all'utente i numeri e li stampo in un array.
     let userAnswer = [];
     for (let i = 0; i < gameNumbers.length; i++) {
         userAnswer.push(parseInt(prompt(`Inserisci un numero che ricordi e premi "Invio".`)));
     }
     
-    console.log(userAnswer);
+    console.log("Risposta utente: ", userAnswer);
 
+    // Richiamo la funzione che confronta gli array.
     const correctContainer = compareArray(gameNumbers, userAnswer);
     console.log(correctContainer);
 
-    let result = `Hai indovinato ${correctContainer.lenght} numeri su 5. Le risposte corrette sono state: `;
+    // Scrivo il risultato.
+    let result = `Hai indovinato ${correctContainer.length} numeri su 5. Le risposte corrette sono state: `;
     for (let i = 0; i < correctContainer.length; i++) {
         result += `${correctContainer[i]} `;        
-    }
-
+    };
+    
+    // Stampo il risultato in pagina.
     const resultOnPage = document.getElementById("result");
     resultOnPage.innerHTML = result;
     
-}, 3010);
+}, 3100);
 
 // FUNCTIONS
 function getRndInteger(min, max) {
@@ -49,17 +52,17 @@ function getRndInteger(min, max) {
 }
 
 /**
- * Descrizione: la funzione confronta i dati contenuti in un array userArray con quelli contenuti in correctArray.
- * @param {any} correctArray -> sono i dati che userArray dovrebbe contenere.
- * @param {any} userArray -> sono i dati da confrontare con correctArray.
+ * Descrizione: la funzione confronta i dati contenuti in un array nbrsToCompare con quelli contenuti in correctArray.
+ * @param {any} correctArray -> sono i dati che nbrsToCompare dovrebbe contenere.
+ * @param {any} nbrsToCompare -> sono i dati da confrontare con correctArray.
  * @returns {array} -> la funzione restituisce un array contenente i valori che matchano. 
  */
-function compareArray (correctArray, userArray) {
+function compareArray (correctArray, nbrsToCompare) {
     let correctAnswers = [];
     for (let i = 0; i < correctArray.length; i++) {
-        if (userArray.includes(correctArray[i])) {
+        if (nbrsToCompare.includes(correctArray[i])) {
             correctAnswers.push(correctArray[i]);
         }    
     }
-    return correctAnswers
+    return correctAnswers;
 }
