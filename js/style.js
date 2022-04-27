f0edd786ea83647c1064a7ab101adf03cb5d5719
matte887 <m.erroi92@gmail.com>
2022-04-27 14:50:33 +0200

    Aggiunti num random e tolti con timer. Chiesti numeri

---
+++ js/style.js
@@ -0,0 +1,36 @@
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// Creo cinque numeri casuali da 1 a 100 e li stampo in pagina.
+let gameNumbers = [];
+
+for (let i = 0; i < 5; i++) {
+    const randomNumber = getRndInteger(1, 100);
+    gameNumbers.push(randomNumber);
+}
+
+const numbersContainer = document.getElementById("numbers");
+
+numbersContainer.innerHTML = `Memorizza questi numeri: ${gameNumbers[0]}, ${gameNumbers[1]}, ${gameNumbers[2]}, ${gameNumbers[3]}, ${gameNumbers[4]}`;
+
+// Imposto una funzione di timeout. Allo scadere del tempo aggiungo una classe che fa sparire i numeri.
+setTimeout(function(){
+    numbersContainer.classList.add("hide");
+}, 3000);
+
+// Chiedo all'utente i numeri
+setTimeout(function(){
+    let userAnswer = [];
+    for (let i = 0; i < gameNumbers.length; i++) {
+        userAnswer.push(parseInt(prompt(`Inserisci un numero che ricordi e premi "Invio".`)));
+    }
+    
+    console.log(userAnswer);
+
+}, 3010);
+
+// FUNCTIONS
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
